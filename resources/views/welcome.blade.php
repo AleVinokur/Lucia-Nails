87cbfb409977b55c3350ba51f4ab897908d2c1e4
Alejandro Vinokur <ajvvja13@gmail.com>
2023-08-07 23:08:08 -0300

    Inicio proyecto e-commerce de Lu

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nombre del Emprendimiento</title>
+    <!-- Agrega los enlaces a los estilos de Bootstrap si no los has incluido ya -->
+    <!-- <link href="ruta/de/tu/bootstrap.min.css" rel="stylesheet"> -->
+</head>
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <!-- Logo y nombre del emprendimiento -->
+            <a class="navbar-brand" href="#">
+                <img src="ruta/del/logo.png" alt="Logo" width="40" height="40">
+                Nombre del Emprendimiento
+            </a>
+
+            <!-- Carrito -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-shopping-cart"></i> Carrito
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Resto del contenido de tu página -->
+    <div class="container">
+        <h1>Bienvenido a nuestro sitio</h1>
+        <!-- Aquí puedes agregar más contenido de tu página -->
+    </div>
+
+    <!-- Agrega los enlaces a los scripts de Bootstrap y Font Awesome si no los has incluido ya -->
+    <!-- <script src="ruta/de/tu/bootstrap.min.js"></script> -->
+    <!-- <script src="ruta/de/tu/fontawesome.min.js"></script> -->
+</body>
+</html>
